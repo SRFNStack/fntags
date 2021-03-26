b114e3e00d7e3ea43888af91a3271521928d420d
Robert Kempton <r@snow87.com>
2021-03-25 23:59:29 -0600

    add some fntags tests

--- src/fntags.mjs
+++ src/fntags.mjs
@@ -484,8 +484,6 @@ const evaluateElement = ( element, value ) => {
 export const renderNode = ( node ) => {
     if( node && typeof node === 'object' && node.then === undefined ) {
         return node
-    } else if( typeof node === 'string' || typeof node === 'number' ) {
-        return document.createTextNode( node )
     } else if( node && typeof node === 'object' && typeof node.then === 'function' ) {
         const temp = marker()
         node.then( el => temp.replaceWith( renderNode( el ) ) ).catch( e => console.error( 'Caught failed node promise.', e ) )
@@ -516,7 +514,7 @@ let setAttribute = function( attrName, attr, element ) {
                 attr[ style ].init( style, element )
                 attr[ style ] = attr[ style ]()
             }
-            element.style[ style ] = attr[style] && attr[ style ].toString()
+            element.style[ style ] = attr[ style ] && attr[ style ].toString()
         }
     } else if( typeof attr === 'function' && attrName.startsWith( 'on' ) ) {
         element.addEventListener( attrName.substring( 2 ), attr )
@@ -530,12 +528,10 @@ let setAttribute = function( attrName, attr, element ) {
 }
 
 export const isAttrs = ( val ) => val !== null && typeof val === 'object' && val.nodeType === undefined && !Array.isArray( val ) && typeof val.then !== 'function'
-/**of children
- * Aggregates all attribute objects from a list
- * @param children
- * @returns {{}} A single object containing all of the aggregated attribute objects
+/**
+ * helper to get the attr object
  */
-export const getAttrs = ( children ) => isAttrs( children[ 0 ] ) ? children[ 0 ] : {}
+export const getAttrs = ( children ) => Array.isArray( children ) && isAttrs( children[ 0 ] ) ? children[ 0 ] : {}
 
 /**
  * A hidden div node to mark your place in the dom
@@ -547,21 +543,17 @@ const marker = ( attrs ) => h( 'div', Object.assign( attrs || {}, { style: 'disp
  * A function to create a component with a pre-defined style.
  * For example, the flex* elements in fnelements.
  *
- * @param attrs
+ * @param style
  * @param tag
  * @param children
  * @return {*}
  */
-export const styled = ( attrs, tag, children ) => {
+export const styled = ( style, tag, children ) => {
     let firstChild = children[ 0 ]
     if( isAttrs( firstChild ) ) {
-        if( firstChild.style ) {
-            Object.assign( firstChild.style, attrs.style )
-        } else {
-            Object.assign( firstChild, attrs )
-        }
+        children[0].style = Object.assign( style, firstChild.style)
     } else {
-        children.unshift( attrs )
+        children.unshift( { style } )
     }
     return h( tag, ...children )
 }
--- test/unit/fntags_spec.js
+++ test/unit/fntags_spec.js
@@ -1,41 +1,42 @@
-import { h } from '../../docs/lib/fntags.mjs'
+import * as fntags from '../../docs/lib/fntags.mjs'
+import { getAttrs, renderNode } from '../../docs/lib/fntags.mjs'
 
 describe( 'fntags', () => {
 
     describe( 'h', () => {
         it( 'should create a an element with the passed in tag', () => {
-            const e = h( 'div' )
+            const e = fntags.h( 'div' )
             expect( e.tagName ).to.eq( 'DIV' )
         } )
         it( 'should set the namespace if specified in the tag', () => {
-            const e = h( 'ns=http://www.w3.org/2000/svg|svg' )
+            const e = fntags.h( 'ns=http://www.w3.org/2000/svg|svg' )
             expect( e.namespaceURI ).eq( 'http://www.w3.org/2000/svg' )
             expect( e.tagName ).eq( 'svg' )
         } )
         it( 'should use the first non-node, object arg as attributes', () => {
-            const e = h( 'div', { id: 'jerry' } )
+            const e = fntags.h( 'div', { id: 'jerry' } )
             expect( e.id ).eq( 'jerry' )
         } )
         it( 'should use not use null as attributes', () => {
-            const e = h( 'div', null )
+            const e = fntags.h( 'div', null )
             expect( e.id ).eq( '' )
         } )
         it( 'should use not use undefined as attributes', () => {
-            const e = h( 'div', undefined )
+            const e = fntags.h( 'div', undefined )
             expect( e.id ).eq( '' )
         } )
         it( 'should use not use arrays as attributes', () => {
-            const e = h( 'div', [{ id: 'jerry' }] )
+            const e = fntags.h( 'div', [{ id: 'jerry' }] )
             expect( e.id ).eq( '' )
         } )
         it( 'should use not use promises as attributes', () => {
             const p = Promise.resolve()
             p.id = 'jerry'
-            const e = h( 'div', p )
+            const e = fntags.h( 'div', p )
             expect( e.id ).eq( '' )
         } )
         it( 'should use not use dom nodes as attributes', () => {
-            const e = h( 'div', h( 'div', { id: 'jerry' } ) )
+            const e = fntags.h( 'div', fntags.h( 'div', { id: 'jerry' } ) )
             expect( e.id ).eq( '' )
         } )
 
@@ -48,7 +49,7 @@ describe( 'fntags', () => {
                 expect( element.tagName ).eq( 'DIV' )
                 initEl = element
             }
-            const e = h( 'div', {
+            const e = fntags.h( 'div', {
                 name: fn
             } )
             expect( e.getAttribute( 'name' ) ).eq( 'jerry' )
@@ -56,38 +57,44 @@ describe( 'fntags', () => {
         } )
 
         it( 'should append all the children passed and expand arrays', () => {
-            const el = h( 'div', h( 'div' ), [h( 'div' ), h( 'div' ), h( 'div' )], h( 'div' ), h( 'div' ), [h( 'div' ), h( 'div' ), h( 'div' )], h( 'div' ) )
+            const el = fntags.h( 'div',
+                                 fntags.h( 'div' ),
+                                 [fntags.h( 'div' ), fntags.h( 'div' ), fntags.h( 'div' )],
+                                 fntags.h( 'div' ),
+                                 fntags.h( 'div' ),
+                                 [fntags.h( 'div' ), fntags.h( 'div' ), fntags.h( 'div' )],
+                                 fntags.h( 'div' ) )
             expect( el.children.length ).eq( 10 )
         } )
         it( 'should convert bad data to text and include any strings', () => {
-            const el = h( 'div', '1', null, '2', undefined, '3', {}, '4' )
+            const el = fntags.h( 'div', '1', null, '2', undefined, '3', {}, '4' )
             expect( el.innerText ).eq( '1null2undefined3[object Object]4' )
         } )
 
         it( 'should set the attribute and element property for value', () => {
-            const el = h( 'input', { value: 'taco' } )
+            const el = fntags.h( 'input', { value: 'taco' } )
             expect( el.value ).eq( 'taco' )
             expect( el.getAttribute( 'value' ) ).eq( 'taco' )
         } )
         it( 'should set disabled, checked, and selected as booleans on the element', () => {
-            const el = h( 'input', { disabled: true, checked: true, selected: true } )
+            const el = fntags.h( 'input', { disabled: true, checked: true, selected: true } )
             expect( el.disabled ).eq( true )
             expect( el.checked ).eq( true )
             expect( el.selected ).eq( true )
         } )
         it( 'should set string and number types as attributes', () => {
-            const el = h( 'input', { id: 1, name: 'taco' } )
+            const el = fntags.h( 'input', { id: 1, name: 'taco' } )
             expect( el.getAttribute( 'id' ) ).eq( '1' )
             expect( el.getAttribute( 'name' ) ).eq( 'taco' )
         } )
 
         it( 'should set namespaced attributes as ns attributes', () => {
-            const el = h( 'input', { 'ns=http://www.w3.org/1999/xlink|xlink:href': '/foo/bar' } )
+            const el = fntags.h( 'input', { 'ns=http://www.w3.org/1999/xlink|xlink:href': '/foo/bar' } )
             expect( el.getAttributeNS( 'http://www.w3.org/1999/xlink', 'href' ) ).eq( '/foo/bar' )
         } )
 
         it( 'should use a style string correctly', () => {
-            const el = h( 'marquee', {
+            const el = fntags.h( 'marquee', {
                 style: 'color: blue; font-size:50px; border:2px;'
             }, 'weee' )
             expect( el.style.color ).eq( 'blue' )
@@ -96,7 +103,7 @@ describe( 'fntags', () => {
         } )
 
         it( 'should use a style object correctly', () => {
-            const el = h( 'marquee', {
+            const el = fntags.h( 'marquee', {
                 style: {
                     color: 'blue',
                     'font-size': '50px',
@@ -117,7 +124,7 @@ describe( 'fntags', () => {
                 expect( element.tagName ).eq( 'MARQUEE' )
                 initEl = element
             }
-            const el = h( 'marquee', {
+            const el = fntags.h( 'marquee', {
                 style: {
                     color: fn,
                     'font-size': '50px',
@@ -132,27 +139,103 @@ describe( 'fntags', () => {
         it( 'should add an event listener when the attr is a function and the attr name starts with on', () => {
             let clicked = false
             const fn = () => clicked = true
-            const el = h( 'marquee', {
+            const el = fntags.h( 'marquee', {
                 onclick: fn
             }, 'weee' )
             el.click()
             expect( clicked ).eq( true )
         } )
 
-        it('should set weird values as attributes', ()=>{
-            const el = h( 'marquee', {
-                style: {color:null},
+        it( 'should set weird values as attributes', () => {
+            const el = fntags.h( 'marquee', {
+                style: { color: null },
                 'data-blue': true,
                 null: undefined,
-                name: {nameio:'jello'},
+                name: { nameio: 'jello' },
                 'ns=http://www.w3.org/1999/xlink|xlink:href': null
             }, 'weee' )
             expect( el.style.color ).eq( '' )
-            expect( el.getAttribute('data-blue')).eq( 'true' )
-            expect( el.getAttribute('null')).eq( 'undefined' )
-            expect( el.getAttribute('name')).eq( '[object Object]' )
+            expect( el.getAttribute( 'data-blue' ) ).eq( 'true' )
+            expect( el.getAttribute( 'null' ) ).eq( 'undefined' )
+            expect( el.getAttribute( 'name' ) ).eq( '[object Object]' )
             expect( el.getAttributeNS( 'http://www.w3.org/1999/xlink', 'href' ) ).eq( 'null' )
-        })
+        } )
 
+    } )
+
+    describe( 'renderNode', () => {
+        it( 'assumes a node if it\'s a truthy object without a then function', () => {
+            let o = { id: 1 }
+            let n = fntags.renderNode( o )
+            expect( n ).eq( o )
+        } )
+        it( 'converts strings and numbers into text nodes', () => {
+            let a = fntags.renderNode( 'a' )
+            let i = fntags.renderNode( 1 )
+            expect( a ).instanceOf( Text )
+            expect( a.wholeText ).eq( 'a' )
+            expect( i ).instanceOf( Text )
+            expect( i.wholeText ).eq( '1' )
+        } )
+
+        it( 'resolves promise results into a node', async() => {
+            let el = renderNode( Promise.resolve( 'hi' ) )
+            expect( el.tagName ).eq( 'DIV' )
+            expect( el.style.display ).eq( 'none' )
+            let done
+            let p = new Promise( ( res ) => {
+                done = res
+            } )
+            el.replaceWith = ( rendered ) => {
+                expect( rendered ).instanceOf( Text )
+                expect( rendered.wholeText ).eq( 'hi' )
+                done()
+            }
+            await p
+        } )
+
+        it( 'calls functions and creates nodes from their values', () => {
+            let el = renderNode( () => 'hi' )
+            expect( el ).instanceOf( Text )
+            expect( el.wholeText ).eq( 'hi' )
+        } )
+
+        it( 'turns other stuff into test nodes', () => {
+            let el = renderNode( null )
+            expect( el ).instanceOf( Text )
+            expect( el.wholeText ).eq( 'null' )
+        } )
+
+    } )
+
+    describe( 'getAttrs', () => {
+        it( 'returns the first element of the passed array if it\'s an attributes object', () => {
+
+        } )
+        it( 'doesn\'t have time for your bs', () => {
+            expect( getAttrs( null ) ).empty
+            expect( getAttrs( undefined ) ).empty
+            expect( getAttrs( {} ) ).empty
+            expect( getAttrs( () => {} ) ).empty
+            expect( getAttrs( 0 ) ).empty
+            expect( getAttrs( NaN ) ).empty
+            expect( getAttrs( Infinity ) ).empty
+        } )
+    } )
+
+    describe( 'styled', () => {
+        it( 'adds the style to the element', () => {
+            let s = fntags.styled( { color: 'blue' }, 'div', [] )
+            expect(s.style.color).eq('blue')
+        } )
+        it( 'adds the style to an element with an existing style', () => {
+            let s = fntags.styled( { color: 'blue' }, 'div', [{style: {'font-size':'10px'}}] )
+            expect(s.style.color).eq('blue')
+            expect(s.style['font-size']).eq('10px')
+        } )
+        it( 'allows overwriting the style', () => {
+            let s = fntags.styled( { color: 'blue' }, 'div', [{style: {color:'purple'}}] )
+            expect(s.style.color).eq('purple')
+        } )
     } )
 } )
